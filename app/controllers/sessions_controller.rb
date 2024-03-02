cdb03182d1f944be6f1921364c8a2f456df84cd1
oota mahiro <mkz.1210@icloud.com>
2024-03-03 01:35:09 +0900

    user新規登録、ログイン

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+  def new
+  end
+  
+  def create
+    user = User.find_by(email: params[:session][:email])
+    if user && user.authenticate(params[:session][:password])
+      # ログイン成功時の処理
+      log_in user
+      redirect_to favs_path, notice: "Logged in successfully"
+    else
+      flash.now.alert = "Email or password was invalid"
+      render :new
+    end
+  end
+  
+  def destroy
+    log_out
+    redirect_to root_path, notice: "Logged out!"
+  end
+end
