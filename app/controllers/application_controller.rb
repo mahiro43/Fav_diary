ca459263c98babb55dd00c7d1b7e0ecaeedbed2f
oota mahiro <mkz.1210@icloud.com>
2024-07-03 13:42:20 +0900

    タイトル変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   include SessionsHelper
-  helper_method :log_in, :current_user, :logged_in?
+  helper_method :log_in, :current_user,:current_user?, :logged_in?
 
   add_flash_types :success, :info, :warning, :danger
   
@@ -10,6 +10,10 @@ class ApplicationController < ActionController::Base
     
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def current_user?(user)
+    user == current_user
   end
     
   def logged_in?
